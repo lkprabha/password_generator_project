457f847d61970e84bdc361ed84b570aa42f7f2a8
lkprabha <prasliit18@gmail.com>
2021-03-28 23:33:00 +0800

    utomatic random password generator module :

---
+++ password_generator/generator/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+# Create your views here.
+
+def home(request):
+    return render(request,'generator/home.html', {'password' :"fdfdfdfdfdfd"})
+
+def password(request):
+
+    charactors=list('abcdefghijklmnopqrstuvwxyz')
+
+    if request.GET.get('uppercase'):
+        charactors.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('Special Charactors'):
+        charactors.extend(list('!@#%$&'))
+
+    if request.GET.get('Numbers'):
+        charactors.extend(list('0123456789'))
+
+    length= int(request.GET.get('length',12))
+    thepassword=''
+
+    for x in range(length):
+        thepassword += random.choice(charactors)
+
+    return render(request,'generator/password.html',{'password': thepassword})
